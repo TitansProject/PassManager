2bb740a6566f0a179610993e06c89964d63aaed8
Albert Eduard Merino Pulido <albertmerino11@gmail.com>
2017-11-24 23:51:19 +0100

    Adding wifi model. Show list of wifis on index

--- PassManagerApp/views.py
+++ PassManagerApp/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.template import loader
+
+from PassManagerApp.models import Wifi
+
+
+def index(request):
+    wifis = Wifi.objects.all()
+    template = loader.get_template('PassManagerApp/index.html')
+    context = {
+        'wifis': wifis,
+    }
+    return HttpResponse(template.render(context, request))
